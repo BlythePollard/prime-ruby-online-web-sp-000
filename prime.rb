fcb8fe8a721294af5d1fbd704f0711fe9359c37d
Blythe Pollard <blytheepollard@gmail.com>
2019-09-13 15:38:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,9 @@
 def prime?(value)
-  Prime.prime?(value)
+     return false if value < 2
+    (2..value - 1).each do |x|
+        if (value % x) == 0
+            return false
+        end
+    end
+    true
 end
